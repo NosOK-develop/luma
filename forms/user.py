a5f3ac82aee175f17e6b16f8fe8be904135f86a4
NosOK-develop <d877231@gmail.com>
2026-05-31 12:01:40 +0700

    Update user.py

--- forms/user.py
+++ forms/user.py
@@ -21,8 +21,8 @@ class EditProfileForm(FlaskForm):
     )
     password = StringField(
         'Пароль',
-        validators=[DataRequired("Введите новый или старый пароль, это обязательно"), Length(min=6, max=20), "placeholder": "Введите новый или старый пароль"],
-        render_kw={"class": "form-control bg-secondary text-white border-0"}
+        validators=[DataRequired("Введите новый или старый пароль, это обязательно"), Length(min=6, max=20)],
+        render_kw={"class": "form-control bg-secondary text-white border-0", "placeholder": "Введите новый или старый пароль"}
     )
     about = TextAreaField(
         'О себе',
